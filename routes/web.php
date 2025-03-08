4dd92be44990c9d312bd8f6766d2459c118b0cd5
faisalahammed044274 <faisalahammed044274@gmail.com>
2025-03-08 16:21:03 +0600

    Named Route

--- routes/web.php
+++ routes/web.php
@@ -1,3 +1,12 @@
+<style>
+    h3 {
+    padding: 30px;
+    background: red;
+    border-radius: 12px;
+    color: white;
+    margin: 30px;
+    }
+</style>
 <?php
 
 use Illuminate\Support\Facades\Route;
@@ -13,11 +22,37 @@ Route::get('/', function () {
     return view('welcome', ['person' => $person]);
 });
 
+Route::get('/user/{username}', function ($username) {
+    return "Username: $username";
+})->where('username', '[a-z]+');
+
+Route::get("{lang}/product/{id}", function($lang, $id){
+    return "<h3>Language: $lang, Product ID: $id</h3>";
+})->where(['lang' => '[a-z]{2}', 'id' => '[0-9]+']);
+
 
 Route::get('/about', function () {
     return view('about');
-});
+})->name('about');
+
+$aboutPageUrl = route('about');
+dd($aboutPageUrl);
+
+Route::get('/contact', function () {
+    return view('contact');
+})->name('contact');
+
+$contactPageUrl = route('contact');
+dd($contactPageUrl);
 
 Route::get('/product/{id}', function ($id) {
-    return "Product ID: " . $id;
-});
+    return "Product ID: $id";
+})->whereNumber('id');
+
+Route::get('/product-category/{category?}', function ($category) {
+    return "Product Category: $category";
+})->whereAlpha('category');
+
+Route::get('/search/{search}', function ($search) {
+    return "Search: $search";
+})->where('search', '.*');
